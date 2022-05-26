51ee13d36a2784ba6e791e52b66da2bed736af2c
Germano Kuerten <germanokuerten@gmail.com>
2022-05-25 20:45:29 -0700

    index updated

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,21 @@
-export default function Index () {
-    return (
-        <h1> Index </h1>
-    )
+import { Link } from "react-router-dom"
+
+export default function Index ({people, createPeople}) {
+
+    // loaded function
+    const loaded = () => {
+    return people.map(person => (
+      <div key={person._id} className="person">
+        <Link to={`/people/${person._id}`}>
+          <h1>{person.name}</h1>
+        </Link>
+        <img src={person.image} alt={person.name} />
+        <h3>{person.title}</h3>
+      </div>
+    ))
+  }
+  const loading = () => <h1>Loading...</h1>
+
+  return people ? loaded() : loading()
 }
+
