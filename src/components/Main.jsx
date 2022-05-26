a814d5a25fe4b951071e18cc19246d75093aba99
Germano Kuerten <germanokuerten@gmail.com>
2022-05-25 20:13:06 -0700

    main component updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,42 @@
+import { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import Index from "../pages/Index"
 import Show from "../pages/Show"
 
 
 export default function Main () {
+
+    const [people, setPeople] = useState(null)
+
+    const URL = "https://gk-fullstack-mern-app-backend.herokuapp.com/people/"
+
+    const getPeople = async () => {
+        const data = await fetch(URL).then(res => res.json())
+        setPeople(data)
+    }
+
+    const createPeople = async person => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+              "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(person),
+        })
+        // update list of people
+        getPeople()
+    }
+
+    useEffect(() => {
+        getPeople()
+    }, [])
+       
+
     return (
         <main>
             <Routes>
-                <Route exact path="/" element={<Index />} />
-                <Route path="/people/:id" element={<Show />} />
+                <Route exact path="/" element={<Index people={people} createPeople={createPeople} />} />
+                <Route path="/people/:id" element={<Show people={people} />} />
             </Routes>
         </main>
     )
