1e0678f8c7522ff225e062dbc96de5045601670f
Germano Kuerten <germanokuerten@gmail.com>
2022-05-25 19:22:20 -0700

    set up client side routes

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+
+
+export default function Main () {
+    return (
+        <main>
+            <Routes>
+                <Route exact path="/" element={<Index />} />
+                <Route path="/people/:id" element={<Show />} />
+            </Routes>
+        </main>
+    )
+}
